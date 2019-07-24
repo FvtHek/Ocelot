443041dda37a85303547301a6c60057923c9d6e4
SGANigz <frans_vanthek@live.nl>
2019-07-25 01:33:56 +0200

    Fixed show server status in bot status on discord

--- Ocelot/Commands/mcstatus.js
+++ Ocelot/Commands/mcstatus.js
@@ -1,52 +1,18 @@
-exports.run = async (Ocelot, msg, args) => {
-function update() {
-  /*seconds = seconds + 1;
-  secondsString = seconds.toString();
-  Ocelot.user.setActivity(secondsString, { type: 'Playing' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);*/
+const Discord = require("discord.js");
+const config = require('../config.json');
+const mcping = require("mc-ping-updated");
 
-    mcping(mcIP, mcPort, function(err, res, body) {
-      if(err) {
-          console.log(err);
-          //return message.reply('Error getting Minecraft server status...');
-      }
-      body = JSON.parse(body);
-      var status = 'Server offline';
-      console.log(body.motd);
-      if(body.online) {
-          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-            Ocelot.user.setStatus('idle')
-            //.then(console.log)
-            .catch(console.error);
-          }else{
-            Ocelot.user.setStatus('online')
-            //.then(console.log)
-            .catch(console.error);
-          }
-            if(body.players.now) {
-                status = ' ' + body.players.now + '  of  ' + body.players.max;
-              } else {
-                status = ' 0  of  ' + body.players.max;
-        }
-      } else {
-        Ocelot.user.setStatus('dnd')
-        //.then(console.log)
-        .catch(console.error);
-
-      }
-      Ocelot.user.setActivity(status, { type: 'PLAYING' })
-      .then(presence => console.log(status))
-      .catch(console.error);
+module.exports.run = async (Ocelot, msg, args) => {
+  mcping(config.ip, config.port, function(err, res) {
+    if(err) {
+      console.error(err);
+    } else {
+      msg.channel.send("```json" + JSON.stringify(res) + "```");
+      console.log(res);
+    }
   });
-
 }
-Ocelot.on("ready", () => {
-  console.log("I am ready!");
-  Ocelot.setInterval(update,300);
-});
 
-}
 module.exports.help = {
-    name: "mcstatus"
+  name: "mcstatus"
 }
--- Ocelot/index.js
+++ Ocelot/index.js
@@ -1,13 +1,14 @@
 const config = require('./config.json');
+var mcping = require('mc-ping-updated');
 const Discord = require('discord.js');
 const fs = require('fs');
 const Ocelot = new Discord.Client({disableEveryone: true});
 Ocelot.commands = new Discord.Collection();
-var mcping = require('mc-ping-updated');
+
 var statustring = "No signal";
 var mcCommand = '/minecraft'; // Command for triggering
-var mcIP = config.ip; // Your MC server IP
-var mcPort = config.port; // Your MC server port
+
+Ocelot.login(config.token);
 
 fs.readdir("./Commands/", (err, files) => {
     if(err) console.log(err);
@@ -26,7 +27,8 @@ fs.readdir("./Commands/", (err, files) => {
 
 Ocelot.on("ready", async () => {
     console.log(`${Ocelot.user.username} is active! I am in ${Ocelot.guilds.size} Server(s)!`)
-    Ocelot.setInterval(update,300);
+    update();
+    Ocelot.setInterval(update,30000);
 });
 
 Ocelot.on("message", async message => {
@@ -43,43 +45,36 @@ if(commandfile) commandfile.run(Ocelot,message,args);
 
 });
 
-function update() {
-    /*seconds = seconds + 1;
-    secondsString = seconds.toString();
-    Ocelot.user.setActivity(secondsString, { type: 'Playing' })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-    .catch(console.error);*/
-  
-      mcping(mcIP, mcPort, function(err, res) {
+function update() {  
+    var status = '';
+
+    mcping(config.ip, config.port, function(err, res) {
         if(err) {
-            console.log(err);
+            console.log('The server is currently offline.. :(')
+            status = 'Server offline..'
+            Ocelot.user.setPresence({ game: { name: status }, status: 'dnd' });
             //return message.reply('Error getting Minecraft server status...');
-        }
-        body = JSON.parse(res);
-        var status = 'Server offline';
-        console.log(body.description.text);
-        if(body.online) {
-            if((body.description.text=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-              Ocelot.user.setStatus('idle')
-              //.then(console.log)
-              .catch(console.error);
-            }else{
-              Ocelot.user.setStatus('online')
-              //.then(console.log)
-              .catch(console.error);
+
+        } else {
+            console.log(res);
+            console.log(JSON.stringify(res.description.text));
+
+            if((res.description.text === 'We are under maintenance.')){
+                status = "Under Maintenance..";
+                Ocelot.user.setPresence({ game: { name: status }, status: 'idle' });
+                
+                // Ocelot.user.setStatus('idle')
+                
+            } else {
+                Ocelot.user.setStatus('online')
+                status = ' ' + res.players.online + '  of  ' + res.players.max + " online";
+                Ocelot.user.setActivity(status, { type: 'PLAYING' })
+                .then(presence => console.log(status))
+                .catch(console.error);
             }
-              if(body.players.now) {
-                  status = ' ' + body.players.online + '  of  ' + body.players.max;
-                } else {
-                  status = ' 0  of  ' + body.players.max;
-          }
-        } 
+        }
 
-    Ocelot.user.setActivity(status, { type: 'PLAYING' })
-    .then(presence => console.log(status))
-    .catch(console.error);
+        
     });
-  
-  }
+}
 
-Ocelot.login(config.token);
