9077a6fd2747fd10aaf6d27aec6e1729840a141a
xCyanGrizzly <sam.agsteribbe@hotmail.com>
2019-07-28 13:23:26 +0200

    Start out with mcusernames list

--- Ocelot/Commands/mcusernames.js
+++ Ocelot/Commands/mcusernames.js
@@ -1,6 +1,14 @@
 module.exports.run = async (Ocelot, msg, args) => {
-    if (message.content.startsWith ("link")) {
+  if (msg.content.startsWith ("link")) {
+    username = msg.content.slice (5);
+    Ocelot.usernames [msg.author.id] = {
+      mcusername: msg.content
     }
+    false.writeFile ("./Files/mcusernames.json", JSON.stringify (Ocelot.usernames, null, 4), err => {
+      if (err) throw err;
+      msg.channel.send ("username for " + msg.author.username + "saved")
+    });
+  }
 }
 
 
