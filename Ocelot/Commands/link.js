fa593586d3212829e16fabe11bcd64f5d3e7e6e2
xCyanGrizzly <sam.agsteribbe@hotmail.com>
2019-07-28 21:46:36 +0200

    Remove console.log and unneccesary msg

--- Ocelot/Commands/link.js
+++ Ocelot/Commands/link.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 module.exports.run = async (Ocelot, message, args) => {
-  console.log("lol");
   let usernames = JSON.parse(fs.readFileSync("./files/mcusernames.json", "utf8"));
       if (message.content.startsWith ("&link")) {
         mcusername = message.content.slice (6);
@@ -11,7 +10,6 @@ module.exports.run = async (Ocelot, message, args) => {
         fs.writeFile ("./Files/mcusernames.json", json, (err) => {
           if (err) throw err;
           message.channel.send ("username for " + message.author.username + " saved");
-          message.channel.send (json);
         });
       };
 }
